589ed4fc7dab25c62955b26289436f0a48a5b588
Λνι <deviser@tuta.io>
2021-10-31 14:58:37 +0530

    Update functions.py

--- userge/utils/functions.py
+++ userge/utils/functions.py
@@ -156,7 +156,7 @@ def check_owner(func):
                 pass
         else:
             await c_q.answer(
-                "Only My Master can Access This !!\n\n     𝘿𝙚𝙥𝙡𝙤𝙮 𝙮𝙤𝙪𝙧 𝙤𝙬𝙣 𝙐𝙎𝙀𝙍𝙂𝙀-𝙓",
+                "Only My Master can Access This !!\n\n     Join @MarineChats...",
                 show_alert=True,
             )
 
